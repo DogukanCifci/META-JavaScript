ac7a17dac969f79ce721a1ad7a36c1d58582978a
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-08-09 12:49:01 +0200

    variable and console.log added

---
+++ index.js
@@ -0,0 +1,34 @@
+// This is a single line comment
+
+/* This is a multi line ,
+multi line comment */
+
+console.log('Hello World') ; //like print() ---> console.log()
+
+var person = "Johson" ;
+
+console.log("Hello", person) ;
+
+var greeting = "Hello" ;
+
+console.log(greeting, person) ;
+
+//Python print , + gibi özellikler ile ayni. Sadece print yerine console.log yapiyoruz.
+
+
+var petDog = "Rex" ;
+var petCat = "Pepper" ;
+console.log(petDog);
+console.log(petCat) ;
+
+console.log("My pet dog's name is: ", petDog);
+console.log("My pet cat's name is: ", petCat) ;
+
+var catSound = "puurr" ;
+var dogSound = "woof" ;
+
+console.log(petDog,"says",dogSound) ;
+console.log(petCat,"says",catSound) ; //Burda hala puurr diycek.
+
+catSound = "Meow" ;
+console.log(petCat,"now says", catSound); //Degistirdikten sonra artik Meow diycek.
