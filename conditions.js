f8ef60062bcd98b94a26d1c1245062e41ca31c30
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-08-10 13:59:01 +0200

    for loops and while loops logic

--- conditions.js
+++ conditions.js
@@ -39,3 +39,58 @@ switch(light){
         console.log("The light is not green, orange or red..") ; 
         break ;
 }
+
+
+
+//Example 
+
+var age = 10 ;
+
+if (age >= 65) {
+    console.log("conditional statements- if else and switch -case") ;
+}
+
+else if (age < 65 && age >= 18) {
+    console.log("Each month you get a salary");
+}
+
+else if (age < 18) {
+    console.log("You get an allowance") ;
+}
+
+else {
+    console.log("The value of the age variable is not numerical") ; //Sayi "10" olarak girilse bile numeric olarak algilar ve bir üstteki ciktiyi verir.
+}
+
+
+//Example 
+
+var day = "Sunday" ;
+
+switch(day) {
+    case 'Monday' :
+        console.log("Do something!") ;
+        break ;
+    case 'Tuesday' :
+        console.log("Do something") ;
+        break ;
+    case 'Wednesday' :
+        console.log("Do soemthing!") ;
+        break ;
+    case 'Thursday' :
+        console.log("Do something!") ;
+        break ;
+    case 'Friday' :
+        console.log("Do soemthing") ;
+        break ;
+    case 'Saturday' :
+        console.log('Do something') ;
+        break ; 
+    case 'Sunday' :
+        console.log("Do something") ;
+        break ;
+    default :
+        console.log("There is no such day! ") ;
+        break ;
+ }
+
---
+++ loops.js
@@ -0,0 +1,49 @@
+// FOR loops
+var i = 0 ; // ich muss es auf jeden Fall schreiben.
+for (var i ; i<4 ; i++) {
+    console.log(i);
+}
+
+console.log("------------")
+
+// WHILE LOOPS
+
+var i = 0 ;
+while (i < 5) {
+    console.log(i) ;
+    i += 1 ;
+}
+
+console.log("------------")
+
+
+//Example 
+// var i yi icerde de tanimlayabilirim.
+
+for (var i = 1 ; i < 4 ; i++) {
+    console.log(i) ;
+}
+
+console.log("GO!")
+console.log("----------")
+
+
+// with for loops
+
+for (var i = 10 ; i > 0 ; i--) {
+    console.log(i) 
+}
+
+console.log("Happy New Year  with for loops!!!") ;
+
+
+// with while loops 
+
+var counter = 10 ;
+
+while (counter > 0) {
+    console.log(counter) ;
+    counter -= 1 ;
+}
+
+console.log("Happy new year with while loops")
