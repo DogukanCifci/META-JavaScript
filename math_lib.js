f1e74270de827f6f541bbc032da47a2e8147aa7b
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-08-12 11:30:29 +0200

    math lib. added

---
+++ math_lib.js
@@ -0,0 +1,34 @@
+ // Math büyük harf ile yazilir.
+
+var a = Math.ceil(13.2) ; //Bir büyük sayiya yuvarlar. 
+console.log(a) ; 
+
+
+var b = Math.floor(12.7) ;  //12 ye  yuvarlar.
+console.log(b) ;
+
+
+var c = Math.round(12.3) ; //0.5 den düsükse alt tarafa yüksekse üst tarafa yuvarlar.
+var d = Math.round(14.5) ;
+console.log(c) ;
+console.log(d) ;
+
+
+var f = Math.trunc(123.656) ; //kesirli tarafi siler sadece integer kismi kalir.
+console.log(f) ;
+
+
+console.log(Math.sqrt(9)) ; // 3 Karekök
+
+console.log(Math.pow(2,3)) ;// 2^3 üzeri
+
+console.log(Math.cbrt(8)) ;// 2 ( 8 in kök3 ü )  
+
+console.log(Math.abs(-15)) ; // 15
+
+
+console.log(Math.min(15,7,9)) ; //7
+
+console.log(Math.max(23,31,5)) ; // 31
+
+console.log(Math.cos(30)) ;  
